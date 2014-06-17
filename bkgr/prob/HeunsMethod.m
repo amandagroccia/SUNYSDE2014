4116d4b4811cd89cc7eddfc15fb942d6b7ec1e85
KellyBlack <kjblack@gmail.com>
2014-06-17 15:18:40 -0400

    Set up the huen's trial to use a vector of dt's.

--- bkgr/prob/HeunsMethod.m
+++ bkgr/prob/HeunsMethod.m
@@ -2,21 +2,18 @@
 % For the system:    y' = ry,   y(0) = 1   over the interval [0,T]
 %% True Solution
 clear all
-N=10; T=1; r=1; % N is # of approximations to make for y(T)
+N=500; T=1; r=1; % N is # of approximations to make for y(T)
 dt=zeros(N,1);
 %c=10; % constant interval of increase for dt
 %n=zeros(N,1);
-for i=1:N
-    dt(i)=T/i;
-end
+dt = T./(30:10:N);
 y0=1; k=y0; % ytrue(1)=y(0) init. condition
 ytrue=k*exp(r*T);
 %% Heun's Approximation 
 % yheun=zeros(N+1,1); yheun(1)=ytrue(1);
 j=1;
-errheun=zeros(N+1,1);
-errheun(1)=0;
-while (j<=N)
+errheun=zeros(size(dt,2),1);
+while (j<=size(dt,2))
     yheun=y0;
     i=2;
     while (i<=N+1)
@@ -24,26 +21,25 @@ while (j<=N)
         yheun=yheun+.5*dt(j)*(k1+k2);
         i=i+1;
     end
-    errheun(j+1)=abs(yheun-ytrue);
+    errheun(j)=abs(yheun-ytrue);
     j=j+1;
 end
 %loglog([0:c:dt(N)],errheun,'r*')
 %% Euler's Method
 %yeuler=zeros(N+1,1); yeuler(1)=ytrue(1); 
-erreul=zeros(N+1,1);
-erreul(1)=0;
+erreul=zeros(size(dt,2),1);
 j=1;
-while (j<=N)
+while (j<=size(dt,2))
     yeuler=y0;
     i=2;
     while (i<=N+1)
         yeuler=yeuler+dt(j)*r*yheun;
         i=i+1;
     end
-    erreul(j+1)=abs(yeuler-ytrue); 
+    erreul(j)=abs(yeuler-ytrue); 
     j=j+1;
 end    
-loglog([0;dt],errheun,'r*',[0;dt],erreul,'*'); %hold off
+loglog(dt,errheun,'r*',dt,erreul,'*'); %hold off
 xlabel('dt')
 ylabel('error')
 legend('Heuns','Eulers')
