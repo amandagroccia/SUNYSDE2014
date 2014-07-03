eb4f7e209efd0bfa7f074255a76821055b7f4fb4
KellyBlack <kjblack@gmail.com>
2014-07-03 08:35:20 -0400

    Fixed the null cline graph and added the vector field

--- bkgr/prob/ShrimpNullclines.R
+++ bkgr/prob/ShrimpNullclines.R
@@ -6,14 +6,47 @@ D <- 7
 alpha <- .3
 beta <-.2
 
+par(xpd=FALSE)
 x1 <- seq(0, 1.1, by=.05)
-y1 <- ((1-x1)-gamma/(x1+D))     ### x null-cline
-plot(x1,y1, xlim=c(0,1.1),ylim=c(0,1.1),col=2,type="l")
+y1 <- (x1*(1-x1)-gamma*x1/(x1+D))/alpha     ### x null-cline
+plot(x1,y1, xlim=c(0,1.1),ylim=c(0,1.1),col=2,type="l",
+     main="Phase Plane for The Killer Shrimp",
+     xlab="Species x",ylab="Species y")
 points (c(0,0),c(0,1.1),col=2,type="l")
 
 y2 <- seq(0,1.1,by=.005)
-x2 <- (rho*(1-y2)-delta/(y2+R))     ### y null-cline
+x2 <- (rho*y2*(1-y2)-delta*y2/(y2+R))/beta  ### y null-cline
 points (x2,y2,col=3,type="l")
-points (c(0,1.1),c(0,0),col=3,type="l")      
+points (c(0,1.1),c(0,0),col=3,type="l")
 
-legend (.8,.8,c("xnullcline","ynullcline"),col=c(2,3), lty=c(1,1))
+par(xpd=TRUE)
+
+legend (.8,1.26,c("x Nullcline","y Nullcline"),col=c(2,3), lty=c(1,1))
+
+vectorField <- function(x,y,alpha,beta,delta,gamma,rho,R,D)
+    {
+        fx <- x*x*(1-x)-alpha*x*y-gamma*x*x/(D+x)
+        fy <- rho*y*y*(1-y)-beta*x*y-delta*y*y/(R+y)
+        return(c(fx,fy))
+    }
+
+x1 <- seq(0, 1.1, by=.033)
+y2 <- x1
+vx <- numeric(length(x1))
+vy <- numeric(length(y2))
+dt <- 0.01;
+for (x in x1)
+{
+    lupe <- 0
+    for (y in y2)
+        {
+            lupe <- lupe + 1
+            v <- vectorField(x,y,alpha,beta,delta,gamma,rho,R,D)
+            vx[lupe] <- v[1]
+            vy[lupe] <- v[2]
+        }
+    vecLength = sqrt(vx*vx+vy*vy)
+    endx <- x + vx*dt/vecLength
+    endy <- y2 + vy*dt/vecLength
+    arrows(x,y2, endx, endy,length=dt*3, col=1,angle=20);
+}
