18f97b98a1b3ef9b55f4ee0f7eacb6ff0158360f
CarrieRider <carrie.n.rider@gmail.com>
2014-07-03 10:44:22 -0400

    Newton Code and Update on Nullclines

--- bkgr/prob/ShrimpNullclines.R
+++ bkgr/prob/ShrimpNullclines.R
@@ -3,18 +3,22 @@ gamma <- .8
 rho <- 1
 R <- 3
 D <- 7
-alpha <- .3
+alpha <- .2
 beta <-.2
 
+xmin <- 0
+xmax <- 1.1
+ymin <- 0
+ymax <- 1.1
 par(xpd=FALSE)
-x1 <- seq(0, 1.1, by=.05)
+x1 <- seq(xmin, xmax, by=.01)
 y1 <- (x1*(1-x1)-gamma*x1/(x1+D))/alpha     ### x null-cline
-plot(x1,y1, xlim=c(0,1.1),ylim=c(0,1.1),col=2,type="l",
+plot(x1,y1, xlim=c(xmin, xmax),ylim=c(ymin, ymax),col=2,type="l",
      main="Phase Plane for The Killer Shrimp",
      xlab="Species x",ylab="Species y")
 points (c(0,0),c(0,1.1),col=2,type="l")
 
-y2 <- seq(0,1.1,by=.005)
+y2 <- seq(ymin, ymax, by=.01)
 x2 <- (rho*y2*(1-y2)-delta*y2/(y2+R))/beta  ### y null-cline
 points (x2,y2,col=3,type="l")
 points (c(0,1.1),c(0,0),col=3,type="l")
