56d55443a6eceb1a0e1b0f3c1de95b37358689b1
Tatyania Moorehead <t.a.moorehead@spartans.nsu.edu>
2014-07-08 15:34:10 -0400

    added more comments to the code

--- bkgr/prob/ConfidenceIntervals .R
+++ bkgr/prob/ConfidenceIntervals .R
@@ -1,19 +1,26 @@
-y <- read.csv("sillydata.csv")
-z <- table(as.factor(y$x))
-N <- sum(z)
+y <- read.csv("sillydata.csv") #reads in the file (this file is made up)
+z <- table(as.factor(y$x)) #puts the numbers into a table for the different categories 
+N <- sum(z) #sums up the values for each category 
 
-p1 <- z[1]/N
-p2 <- z[2]/N
-p3 <- z[3]/N
+#category 1: species 1 lives while species 2 dies out
+#category 2: species 2 lives while species 1 dies out
+#category 3: species 1 and species 2 lives
 
-s12 <- p1*(1-p1)/z[1] #s1 squared 
-s22 <- p2*(1-p2)/z[2] #s2 squared 
-s32 <- p3*(1-p3)/z[3] #s3 squared 
+p1 <- z[1]/N #approximation of probability 1: the number of trials where species 1 lives divided by the whole population 
+p2 <- z[2]/N #approximation of probability 2: the number of trials where species 2 lives divided by the whole population 
+p3 <- z[3]/N #approximation of probability 3: the number of trials where both species live divided by the whole population 
+
+#finding how many of the runs will fall into each category 
+s12 <- p1*(1-p1)/z[1] #s1 squared: category 1
+s22 <- p2*(1-p2)/z[2] #s2 squared: category 2
+s32 <- p3*(1-p3)/z[3] #s3 squared: category 3
   
 l2 <- qchisq(.95,2) #finds l squared for the Chi Squared test 
 
-lci1 <- p1 - sqrt(s12*l2) #lower confidence interval
+#finds lower and higher confidence intervals for the seperate categories
+lci1 <- p1 - sqrt(s12*l2) #lower confidence interval 
 hci1 <- p1 + sqrt(s12*l2) #higher confidence interval 
+#prints the values for the confidence intervals in a sentence form
 print(paste("The confidence interval for p1 is from", lci1, "to", hci1,"."))
 
 lci2 <- p2 - sqrt(s22*l2)
