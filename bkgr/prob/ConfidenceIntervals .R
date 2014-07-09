4ce938940bec2a60722dcb66497b3d9213805e95
Tatyania Moorehead <t.a.moorehead@spartans.nsu.edu>
2014-07-09 17:56:01 -0400

    added descriptions

--- bkgr/prob/ConfidenceIntervals .R
+++ bkgr/prob/ConfidenceIntervals .R
@@ -1,20 +1,21 @@
 y <- read.csv("sillydata.csv") #reads in the file (this file is made up)
-z <- table(as.factor(y$x)) #puts the numbers into a table for the different categories 
+z <- table(as.factor(y$x)) #puts the numbers into a frequency table for the different categories 
 N <- sum(z) #sums up the values for each category 
 #category 1: species 1 lives while species 2 dies out
 #category 2: species 2 lives while species 1 dies out
 #category 3: species 1 and species 2 lives
 
+# calculates sample proportions 
 p1 <- z[1]/N #approximation of probability 1: the number of trials where species 1 lives divided by the whole population 
 p2 <- z[2]/N #approximation of probability 2: the number of trials where species 2 lives divided by the whole population 
 p3 <- z[3]/N #approximation of probability 3: the number of trials where both species live divided by the whole population 
 
-#finding how many of the runs will fall into each category 
+#finding how many of the runs will fall into each category; sample variances for the sample proportions
 s12 <- p1*(1-p1)/z[1] #s1 squared: category 1
 s22 <- p2*(1-p2)/z[2] #s2 squared: category 2
 s32 <- p3*(1-p3)/z[3] #s3 squared: category 3
   
-l2 <- qchisq(.95,2) #finds l squared for the Chi Squared test 
+l2 <- qchisq(.95,2) #finds l squared for the Chi Squared test; inverse Chi Squared 
 
 #finds lower and higher confidence intervals for the seperate categories
 lci1 <- p1 - sqrt(s12*l2) #lower confidence interval 
